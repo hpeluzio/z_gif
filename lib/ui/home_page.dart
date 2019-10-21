a28e23c2a739add777b411679dcf55f69a940ff7
Henrique Peluzio <hpeluzio@gmail.com>
2019-10-20 22:59:43 -0300

    Grid de gifs e carregar mais criado

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -11,17 +11,17 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   String _search;
+  int _offset = 0;
 
   Future<Map> _getGifs() async {
     http.Response response;
-    int _offset;
 
     if (_search == null)
       response = await http.get(
           'https://api.giphy.com/v1/gifs/trending?api_key=0b5mJ9JEwsekheFFRqAewJrVdYR8PfS3&limit=20&rating=G');
     else
       response = await http.get(
-          'https://api.giphy.com/v1/gifs/search?api_key=0b5mJ9JEwsekheFFRqAewJrVdYR8PfS3&q=$_search&limit=20&offset=$_offset&rating=G&lang=en');
+          'https://api.giphy.com/v1/gifs/search?api_key=0b5mJ9JEwsekheFFRqAewJrVdYR8PfS3&q=$_search&limit=19&offset=$_offset&rating=G&lang=en');
 
     return json.decode(response.body);
   }
@@ -56,38 +56,96 @@ class _HomePageState extends State<HomePage> {
                   border: OutlineInputBorder()),
               style: TextStyle(color: Colors.white, fontSize: 18),
               textAlign: TextAlign.center,
+              onSubmitted: (String text) {
+                setState(() {
+                  _search = text;
+                  _offset = 0;
+                });
+              },
             ),
           ),
           Expanded(
             child: FutureBuilder(
-              future: _getGifs(),
-              builder: (context, snapshot) {
-                switch(snapshot.connectionState) {
-                  case ConnectionState.waiting:
-                  case ConnectionState.none:
-                    return Container(
-                      width: 200,
-                      height: 200,
-                      alignment: Alignment.center,
-                      child: CircularProgressIndicator(
-                        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
-                        strokeWidth: 5,
-                      ),
-                    );
-                  default:
-                    if(snapshot.hasError) return Container();
-                    else _createGifTable(context, snapshot);
-                } 
-              }
-            ),
+                future: _getGifs(),
+                builder: (context, snapshot) {
+                  switch (snapshot.connectionState) {
+                    case ConnectionState.waiting:
+                    case ConnectionState.none:
+                      return Container(
+                        width: 200,
+                        height: 200,
+                        alignment: Alignment.center,
+                        child: CircularProgressIndicator(
+                          valueColor:
+                              AlwaysStoppedAnimation<Color>(Colors.white),
+                          strokeWidth: 5,
+                        ),
+                      );
+                    default:
+                      if (snapshot.hasError)
+                        return Container();
+                      else
+                        return _createGifTable(context, snapshot);
+                  }
+                }),
           ),
         ],
       ),
     );
   }
 
-  Widget _createGifTable(BuildContext context, AsyncSnapshot snapshot) {
-    
+  int _getCount(List data) {
+    if (_search == null)
+      return data.length;
+    else
+      return data.length + 1;
   }
 
+  Widget _createGifTable(BuildContext context, AsyncSnapshot snapshot) {
+    return GridView.builder(
+      padding: EdgeInsets.all(10),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+      ),
+      itemCount:
+          _getCount(snapshot.data['data']), //snapshot.data['data'].length,
+      itemBuilder: (context, index) {
+        
+        if (_search == null || index < snapshot.data['data'].length)
+          return GestureDetector(
+            child: Image.network(
+              snapshot.data['data'][index]['images']['fixed_height']['url'],
+              height: 300,
+              fit: BoxFit.cover,
+            ),
+          );
+        else
+          return Container(
+            child: GestureDetector(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Icon(
+                    Icons.add,
+                    color: Colors.white,
+                    size: 70,
+                  ),
+                  Text(
+                    'Carregar mais',
+                    style: TextStyle(color: Colors.white, fontSize: 22),
+                  )
+                ],
+              ),
+              onTap: () {
+                setState(() {
+                  _offset += 19;
+                });
+              },
+            ),
+          );
+      },
+    );
+  }
 }

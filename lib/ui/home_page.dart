4763d96f2bb9c02287fb34031e4f26c4a439515e
Henrique Peluzio <hpeluzio@gmail.com>
2019-10-20 21:24:01 -0300

    Iniciando o layout

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -10,19 +10,20 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   String _search;
 
   Future<Map> _getGifs() async {
     http.Response response;
     int _offset;
 
-    if(_search == null)
-      response = await http.get('https://api.giphy.com/v1/gifs/trending?api_key=0b5mJ9JEwsekheFFRqAewJrVdYR8PfS3&limit=20&rating=G');
+    if (_search == null)
+      response = await http.get(
+          'https://api.giphy.com/v1/gifs/trending?api_key=0b5mJ9JEwsekheFFRqAewJrVdYR8PfS3&limit=20&rating=G');
     else
-      response = await http.get('https://api.giphy.com/v1/gifs/search?api_key=0b5mJ9JEwsekheFFRqAewJrVdYR8PfS3&q=$_search&limit=20&offset=$_offset&rating=G&lang=en');
-  
-    return json.decode(response.body); 
+      response = await http.get(
+          'https://api.giphy.com/v1/gifs/search?api_key=0b5mJ9JEwsekheFFRqAewJrVdYR8PfS3&q=$_search&limit=20&offset=$_offset&rating=G&lang=en');
+
+    return json.decode(response.body);
   }
 
   @override
@@ -36,8 +37,29 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Image.network(
+            'https://developers.giphy.com/static/img/dev-logo-lg.7404c00322a8.gif'),
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.black,
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: TextField(
+              decoration: InputDecoration(
+                  labelText: 'Pesquise aqui',
+                  labelStyle: TextStyle(color: Colors.white),
+                  border: OutlineInputBorder()),
+              style: TextStyle(color: Colors.white, fontSize: 18),
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
